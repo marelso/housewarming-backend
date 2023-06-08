4f696c12c6d18035d44cf4a684f7d14c34778476
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-08 20:06:05 -0300

    Initial create

---
+++ src/main/java/com/tah/housewarming/HouseWarmingApplication.java
@@ -0,0 +1,13 @@
+package com.tah.housewarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseWarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseWarmingApplication.class, args);
+	}
+
+}
