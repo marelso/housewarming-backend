096b26754506f174ed9ba192738cd683e9c5482d
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-08 23:20:37 -0300

    Include tests to see if the product is available.

--- src/test/java/com/tah/housewarming/domain/ProductTest.java
+++ src/test/java/com/tah/housewarming/domain/ProductTest.java
@@ -1,5 +1,6 @@
 package com.tah.housewarming.domain;
 
+import com.tah.housewarming.fixture.ProductFixture;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,25 @@ class ProductTest {
 
     @Test
     public void shouldBeProductAvailable() {
+        var product = ProductFixture.get()
+                .random()
+                .withCount(5)
+                .build();
 
+        var result = product.isProductAvailable();
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldBeProductUnavailable() {
+        var product = ProductFixture.get()
+                .random()
+                .withCount(0)
+                .build();
+
+        var result = product.isProductAvailable();
+
+        assertFalse(result);
     }
 }
