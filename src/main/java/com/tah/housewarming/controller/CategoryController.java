6410c90d598eaaeeea1a643be4261995a756f611
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-09 20:52:52 -0300

    Include get category by id endpoint

--- src/main/java/com/tah/housewarming/controller/CategoryController.java
+++ src/main/java/com/tah/housewarming/controller/CategoryController.java
@@ -1,13 +1,20 @@
 package com.tah.housewarming.controller;
 
+import com.tah.housewarming.domain.Category;
+import com.tah.housewarming.service.CategoryService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/categories")
 @RequiredArgsConstructor
 @Api(tags = "Category", description = "CRUD of categories.")
 public class CategoryController {
+    private final CategoryService service;
+
+    @GetMapping("/{id}")
+    public Category get(@PathVariable("categoryId") Integer id) {
+        return this.service.findById(id);
+    }
 }
