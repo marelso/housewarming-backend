87385d277bceb8b7e8c032bc69bb2eadcc97c175
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-09 01:43:05 -0300

    Create factory to handle string to category parse

--- src/main/java/com/tah/housewarming/factory/CategoryFactory.java
+++ src/main/java/com/tah/housewarming/factory/CategoryFactory.java
@@ -1,2 +1,16 @@
-package com.tah.housewarming.factory;public class CategoryFactory {
+package com.tah.housewarming.factory;
+
+import com.tah.housewarming.domain.Category;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryFactory {
+
+    public Category from(String name) {
+        var category = new Category();
+        category.setName(name);
+
+        return category;
+    }
+
 }
