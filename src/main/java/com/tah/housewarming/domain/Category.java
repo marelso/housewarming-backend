dc20a792ffbc3dfba1bff27bad12e6e5be9223d9
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-08 23:44:36 -0300

    Including spring data annotations to category entity

--- src/main/java/com/tah/housewarming/domain/Category.java
+++ src/main/java/com/tah/housewarming/domain/Category.java
@@ -2,9 +2,15 @@ package com.tah.housewarming.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 
 @Getter
 @Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Category {
     private Integer id;
     private String name;
