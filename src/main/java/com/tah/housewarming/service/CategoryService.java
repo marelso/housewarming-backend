5fc18f3c8201f6de8ede30633da9282ed7153808
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-09 00:49:31 -0300

    Include findAll method in category service.

--- src/main/java/com/tah/housewarming/service/CategoryService.java
+++ src/main/java/com/tah/housewarming/service/CategoryService.java
@@ -1,2 +1,18 @@
-package com.tah.housewarming.service;public class CategoryService {
+package com.tah.housewarming.service;
+
+import com.tah.housewarming.domain.Category;
+import com.tah.housewarming.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return this.repository.findAll();
+    }
 }
