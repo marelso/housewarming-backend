1b520087eadfc54268e6581deb7162bcb6068777
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-09 01:07:29 -0300

    Include new test to exception handling.

--- src/test/java/com/tah/housewarming/service/CategoryServiceTest.java
+++ src/test/java/com/tah/housewarming/service/CategoryServiceTest.java
@@ -1,12 +1,15 @@
 package com.tah.housewarming.service;
 
 
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
-import com.tah.housewarming.domain.Category;
 import com.tah.housewarming.fixture.CategoryFixture;
 import com.tah.housewarming.repository.CategoryRepository;
 import org.junit.jupiter.api.Test;
@@ -18,8 +21,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
@@ -38,6 +39,22 @@ class CategoryServiceTest {
         var result = subject.findAll();
 
         assertThat(result, equalTo(given));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenThereIsNoGivenIdInDatabase() {
+        var id = 1;
+        var given = CategoryFixture.get().random().withId(id).build();
+
+        given(repository.findById(id)).willReturn(Optional.empty());
+
+
+        catchException(() -> subject.findById(id));
+
+
+        assertThat(caughtException(), instanceOf(RuntimeException.class));
+        then(repository).should().findById(id);
+        verifyNoMoreInteractions(repository);
     }
 
     @Test
