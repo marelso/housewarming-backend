e7133b93528154f4c721cd547d141d1811875e0e
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-08 22:33:46 -0300

    Start test development

--- src/test/java/com/tah/housewarming/domain/ProductTest.java
+++ src/test/java/com/tah/housewarming/domain/ProductTest.java
@@ -1,4 +1,13 @@
+package com.tah.housewarming.domain;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ProductTest {
-  
+
+    @Test
+    public void shouldBeProductAvailable() {
+
+    }
 }
