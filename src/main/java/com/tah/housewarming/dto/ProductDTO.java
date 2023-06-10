7a1b94bd3819849d99e25ab2a9cc77576f305ac7
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-10 01:14:42 -0300

    Include new DTO and constructor for Product entity.

--- src/main/java/com/tah/housewarming/dto/ProductDTO.java
+++ src/main/java/com/tah/housewarming/dto/ProductDTO.java
@@ -1,7 +1,16 @@
 package com.tah.housewarming.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
     private Integer id;
     private String name;
