5079ebf3ebdb0aca4e05c8080ada0d4647123d53
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-09 23:33:17 -0300

    Include random method to new fixture

--- src/test/java/com/tah/housewarming/fixture/CreateProductDTOFixture.java
+++ src/test/java/com/tah/housewarming/fixture/CreateProductDTOFixture.java
@@ -2,6 +2,7 @@ package com.tah.housewarming.fixture;
 
 import com.tah.housewarming.domain.Product;
 import com.tah.housewarming.dto.CreateProductDTO;
+import com.tah.housewarming.util.RandomGenerator;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -32,5 +33,13 @@ public class CreateProductDTOFixture {
         );
     }
 
-
+    public CreateProductDTOFixture random() {
+        this.name = RandomGenerator.string();
+        this.description = RandomGenerator.string(654);
+        this.brand = RandomGenerator.string(16);
+        this.count = RandomGenerator.integer(50);
+        this.linksList = RandomGenerator.stringList();
+        this.categories = RandomGenerator.integerList();
+        return this;
+    }
 }
