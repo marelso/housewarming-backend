3945538fe8be18d5fe1ae8ebacf808e1a914aa26
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-09 21:34:00 -0300

    Include find by id method in product service.

--- src/main/java/com/tah/housewarming/service/ProductService.java
+++ src/main/java/com/tah/housewarming/service/ProductService.java
@@ -1,11 +1,23 @@
 package com.tah.housewarming.service;
 
+import com.tah.housewarming.domain.Product;
 import com.tah.housewarming.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository repository;
+
+    public Product findById(Integer id) {
+        return get(id)
+                .orElseThrow(() -> new RuntimeException("There is no product with id: " + id));
+    }
+
+    private Optional<Product> get(Integer id) {
+        return this.repository.findById(id);
+    }
 }
