bd6c198fb2464b0654e1e381d467a5f4f6d3d0d4
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-09 23:41:51 -0300

    Created new dto to be return of product requests

---
+++ src/main/java/com/tah/housewarming/dto/ProductDTO.java
@@ -0,0 +1,13 @@
+package com.tah.housewarming.dto;
+
+import java.util.List;
+
+public class ProductDTO {
+    private Integer id;
+    private String name;
+    private String description;
+    private String brand;
+    private Integer count;
+    private List<String> linksList;
+    private List<String> categories;
+}
