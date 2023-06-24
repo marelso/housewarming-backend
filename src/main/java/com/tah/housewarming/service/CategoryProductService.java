85fc4a3df6f20c3531ca495c60f2eb17c510961f
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-24 04:49:34 -0300

    Including find method at CategoryProductService

--- src/main/java/com/tah/housewarming/service/CategoryProductService.java
+++ src/main/java/com/tah/housewarming/service/CategoryProductService.java
@@ -5,8 +5,14 @@ import com.tah.housewarming.repository.CategoryProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class CategoryProductService {
     private final CategoryProductRepository repository;
+
+    List<CategoryProduct> find(Integer categoryId) {
+        return repository.findAllByCategoryId(categoryId);
+    }
 }
