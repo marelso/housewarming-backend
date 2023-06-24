2c636afc3edf88fdff4f97cafcd11c66c08c88f2
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-24 04:38:52 -0300

    Include mock for repository

--- src/test/java/com/tah/housewarming/service/CategoryProductServiceTest.java
+++ src/test/java/com/tah/housewarming/service/CategoryProductServiceTest.java
@@ -1,7 +1,17 @@
 package com.tah.housewarming.service;
 
+import com.tah.housewarming.repository.CategoryProductRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class CategoryProductServiceTest {
-
+    @Mock
+    private CategoryProductRepository repository;
+    @InjectMocks
+    private CategoryProductService subject;
 }
