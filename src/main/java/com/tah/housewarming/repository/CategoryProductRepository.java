6ffb73eb6bf0f2bd847547bc148a128cbed895fa
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-24 04:46:20 -0300

    Including new seach method at the repo

--- src/main/java/com/tah/housewarming/repository/CategoryProductRepository.java
+++ src/main/java/com/tah/housewarming/repository/CategoryProductRepository.java
@@ -4,6 +4,9 @@ import com.tah.housewarming.domain.CategoryProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryProductRepository extends JpaRepository<CategoryProduct, Integer> {
+    List<CategoryProduct> findAllByCategoryId(Integer categoryId);
 }
