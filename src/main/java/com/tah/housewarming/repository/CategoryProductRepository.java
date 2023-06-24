a0b115c2c189a7535f719ef7acbe3c14622bfd36
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-24 04:29:26 -0300

    Renaming CategoryProduct repo

--- src/main/java/com/tah/housewarming/repository/CategoryProductRepository.java
+++ src/main/java/com/tah/housewarming/repository/CategoryProductRepository.java
@@ -3,5 +3,5 @@ package com.tah.housewarming.repository;
 import com.tah.housewarming.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryProduct extends JpaRepository<Category, Integer> {
+public interface CategoryProductRepository extends JpaRepository<Category, Integer> {
 }
