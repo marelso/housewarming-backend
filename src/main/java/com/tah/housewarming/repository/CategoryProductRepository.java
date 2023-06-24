c54e54625344ebdb962477b7c0de246b76d35003
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-24 04:31:06 -0300

    Include repo annotation

--- src/main/java/com/tah/housewarming/repository/CategoryProductRepository.java
+++ src/main/java/com/tah/housewarming/repository/CategoryProductRepository.java
@@ -2,6 +2,8 @@ package com.tah.housewarming.repository;
 
 import com.tah.housewarming.domain.CategoryProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryProductRepository extends JpaRepository<CategoryProduct, Integer> {
 }
