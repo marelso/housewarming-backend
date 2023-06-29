c623723278651b71c36af46fcf17f0efb4c95963
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-29 04:00:48 -0300

    Include new method at category-product service and line format

--- src/main/java/com/tah/housewarming/service/CategoryProductService.java
+++ src/main/java/com/tah/housewarming/service/CategoryProductService.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -13,7 +14,7 @@ public class CategoryProductService {
     private final CategoryProductRepository repository;
 
     public void create(Integer categoryId, Integer productId) {
-        if(!relationAlreadyExist(categoryId, productId)) {
+        if (!relationAlreadyExist(categoryId, productId)) {
             var relation = new CategoryProduct();
             relation.setCategoryId(categoryId);
             relation.setProductId(productId);
@@ -21,16 +22,27 @@ public class CategoryProductService {
             repository.save(relation);
         }
     }
+
     private Boolean relationAlreadyExist(Integer categoryId, Integer productId) {
         return repository.findAllByCategoryIdAndProductId(categoryId, productId).isPresent();
     }
+
     public List<CategoryProduct> find(Integer categoryId) {
         return repository.findAllByCategoryId(categoryId);
     }
+
     public void deleteByCategory(Integer categoryId) {
         repository.deleteByCategoryId(categoryId);
     }
+
     public void deleteByProduct(Integer productId) {
         repository.deleteByProductId(productId);
+    }
+
+    public List<Integer> findByProductId(Integer productId) {
+        return this.repository.findAllByProductId(productId)
+                .stream()
+                .map(CategoryProduct::getCategoryId)
+                .collect(Collectors.toList());
     }
 }
