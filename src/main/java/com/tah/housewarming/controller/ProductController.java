43b0ce92ffa35637cbcd39b6068586739d6a05ab
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-29 04:04:04 -0300

    Include new Controller to handle product requests.

--- src/main/java/com/tah/housewarming/controller/ProductController.java
+++ src/main/java/com/tah/housewarming/controller/ProductController.java
@@ -1,2 +1,24 @@
-package com.tah.housewarming.controller;public class ProductController {
+package com.tah.housewarming.controller;
+
+import com.tah.housewarming.dto.ProductDTO;
+import com.tah.housewarming.service.ProductService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/products")
+@RequiredArgsConstructor
+@Api(tags = "Product", description = "CRUD of products.")
+public class ProductController {
+    private final ProductService service;
+
+    @GetMapping()
+    public List<ProductDTO> findAll() {
+        return this.service.findAll();
+    }
 }
