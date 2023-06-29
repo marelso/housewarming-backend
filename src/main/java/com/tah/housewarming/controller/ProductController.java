d4c76403b72e6021a67f4468ace5fc36c006d04c
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-06-29 04:10:31 -0300

    Include new method to find products by id.

--- src/main/java/com/tah/housewarming/controller/ProductController.java
+++ src/main/java/com/tah/housewarming/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.tah.housewarming.service.ProductService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class ProductController {
     @GetMapping()
     public List<ProductDTO> findAll() {
         return this.service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProductDTO get(@PathVariable("id") Integer id) {
+        return this.service.findById(id);
     }
 }
