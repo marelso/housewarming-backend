5be7488ab848df0c299e2cc34a354921a4a3f7cb
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-07-07 19:10:04 -0300

    Updated ProductClaim entity to include date properties and constructor anottations

--- src/main/java/com/tah/housewarming/domain/ProductClaim.java
+++ src/main/java/com/tah/housewarming/domain/ProductClaim.java
@@ -1,18 +1,23 @@
 package com.tah.housewarming.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
 @EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductClaim {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +26,7 @@ public class ProductClaim {
     private String username;
     private Boolean available;
     @CreatedDate
-    private Date createdAt;
+    private LocalDateTime createdAt;
     @LastModifiedDate
-    private Date lastModifiedDate;
+    private LocalDateTime lastModifiedDate;
 }
