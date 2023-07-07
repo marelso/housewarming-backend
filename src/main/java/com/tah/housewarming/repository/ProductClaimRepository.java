dc1668dab4066919459b45fa3b51a7a1332891ab
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-07-07 19:08:18 -0300

    Include new method to return all available entities in claim repository

--- src/main/java/com/tah/housewarming/repository/ProductClaimRepository.java
+++ src/main/java/com/tah/housewarming/repository/ProductClaimRepository.java
@@ -2,6 +2,11 @@ package com.tah.housewarming.repository;
 
 import com.tah.housewarming.domain.ProductClaim;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductClaimRepository extends JpaRepository<ProductClaim, Integer> {
+
+    Integer countByProductIdAndAvailableTrue(Integer productId);
+
 }
