12cd75478d9a89dade5ce335af852bd180da6e29
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-07-08 07:47:04 -0300

    Include post and delete endpoints to Product controller

--- src/main/java/com/tah/housewarming/controller/ProductController.java
+++ src/main/java/com/tah/housewarming/controller/ProductController.java
@@ -1,13 +1,19 @@
 package com.tah.housewarming.controller;
 
+import com.tah.housewarming.dto.CreateProductDTO;
 import com.tah.housewarming.dto.ProductDTO;
 import com.tah.housewarming.service.ProductService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -26,5 +32,17 @@ public class ProductController {
     @GetMapping("/{id}")
     public ProductDTO get(@PathVariable("id") Integer id) {
         return this.service.findById(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDTO create(@RequestBody CreateProductDTO product) {
+        return this.service.create(product);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Integer id) {
+        this.service.delete(id);
     }
 }
