31afc6a99339f840d0d6c2eb3589e05b750544c2
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-07-29 19:26:30 -0300

    Including source property to product DTOS

--- src/main/java/com/tah/housewarming/dto/CreateProductDTO.java
+++ src/main/java/com/tah/housewarming/dto/CreateProductDTO.java
@@ -16,7 +16,7 @@ public class CreateProductDTO {
     private String description;
     private String brand;
     private Integer count;
-    private String image;
+    private String source;
     private List<String> linksList;
     private List<Integer> categories;
 }
